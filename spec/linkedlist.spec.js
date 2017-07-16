5ec641c53f9add90c33cc5bd41885a9125da23aa
gunaskr <a.guna14@gmail.com>
2017-07-16 14:00:37 +0530

    linked list node added

--- spec/linkedlist.spec.js
+++ spec/linkedlist.spec.js
@@ -1,6 +1,10 @@
+const Node = require('../app/linkedlist/linkedlist');
+
 describe("linked list",()=>{
 
     it("testing",()=>{
+        const node = new Node("A",null);
+        console.log(JSON.stringify(node));
         expect(1).toBeTruthy();
     })
 })
