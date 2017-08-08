96e16bdf20143780e4493b30d07387d6bb44d8d0
Gunesekar A <gunesekar.a@corp.trizetto.com>
2017-08-08 21:29:37 +0530

    linked list reverse

--- app/linkedlist/linkedlist.js
+++ app/linkedlist/linkedlist.js
@@ -1,10 +1,123 @@
 "use strict";
 
-class Node{
-    constructor(data,next){
+class LinkedList {
+
+    constructor() {
+        this.first = null;
+    }
+
+	/**
+	 * Insert at the front of the list
+	 * @param node
+     * @param position
+	 */
+    insert(n, p = 1) {
+        if (p == 1) {
+            n.setNext(this.first);
+            this.first = n;
+            return;
+        }else {
+            // find the temp node
+            // need to make sure if position is less than size of the list
+            let temp = this.first;
+            for(let i=0;i<p-2;i++){
+                temp = temp.getNext();
+            }
+            //make the temp node as incumbent nodes next
+            n.setNext(temp.getNext());
+            temp.setNext(n);
+        }
+    }
+
+	/**
+	 * Remove from the front of the list
+	 * @param node
+	 */
+    remove() {
+        if (this.first.getNext() != null)
+            this.first = this.first.getNext();
+        else this.first = null;
+    }
+    /**
+     * iteratively reverse the lsit
+     */
+    reverse(){
+        let current,next,prev;
+        current = this.first;
+        prev = null;
+        while(current!=null){
+            next = current.getNext();
+            current.setNext(prev);
+            prev = current;
+            current = next;
+        }
+        this.first = prev;
+    }
+
+    /**
+     * recursively reverse the lsit
+     */
+    reverseRec(p = this.first){
+        if(p.getNext()==null){
+            this.first = p;
+            return;
+        }
+        this.reverseRec(p.getNext());
+        let q = p.getNext();
+        q.setNext(p);
+        p.setNext(null);
+    }
+
+	/**
+	 * Recursively traverse this list and print the node value
+	 * @param node
+	 */
+    printList(node) {
+        console.log("Node is " + node.getValue());
+        if (node.getNext() != null) this.printList(node.getNext());
+    }
+
+    print() {
+        this.printList(this.first);
+    }
+
+    /**
+	 * Recursively traverse this list and print the node value in reverse order
+	 * @param node
+	 */
+    reversePrintList(node) {
+        if (node == null){
+            return;
+        }
+        this.reversePrintList(node.getNext());
+        console.log("Node is " + node.getValue());
+    }
+
+    reversePrint() {
+        this.reversePrintList(this.first);
+    }
+
+}
+
+class Node {
+    constructor(data) {
         this.data = data;
-        this.next = next;
+    }
+
+    setNext(n) {
+        this.next = n;
+    }
+
+    getNext(n) {
+        return this.next;
+    }
+
+    getValue() {
+        return this.data;
     }
 };
 
-module.exports = Node;
+module.exports = {
+    LinkedList: LinkedList,
+    Node: Node
+}
--- spec/linkedlist.spec.js
+++ spec/linkedlist.spec.js
@@ -1,10 +1,88 @@
-const Node = require('../app/linkedlist/linkedlist');
+const LL = require('../app/linkedlist/linkedlist');
 
 describe("linked list",()=>{
 
-    it("testing",()=>{
-        const node = new Node("A",null);
-        console.log(JSON.stringify(node));
+    it("testing node",()=>{
+        const node = new LL.Node("A",null);
         expect(1).toBeTruthy();
+    })
+
+    it("testing linked list insert",()=>{
+        const list = new LL.LinkedList();
+        const node = new LL.Node("A");
+        list.insert(node);
+        const node1 = new LL.Node("B");
+        list.insert(node1);
+        list.print();
+        expect(list.insert).toBeDefined();
+    })
+
+    it("testing linked list insert at specific position",()=>{
+        const list = new LL.LinkedList();
+        const node = new LL.Node("A");
+        list.insert(node);
+        const node1 = new LL.Node("B");
+        list.insert(node1);
+        const node2 = new LL.Node("C");
+        list.insert(node2,2);
+        list.print();
+        expect(list.insert).toBeDefined();
+    })
+
+    it("testing linked list remove",()=>{
+        const list = new LL.LinkedList();
+        const node = new LL.Node("A");
+        list.insert(node);
+        const node1 = new LL.Node("B");
+        list.insert(node1);
+        const node2 = new LL.Node("C");
+        list.insert(node2);
+        list.remove();
+        list.print();
+        expect(list.insert).toBeDefined();
+    })
+
+     it("testing linked list reverse",()=>{
+         console.log("testing linked list reverse");
+        const list = new LL.LinkedList();
+        const node = new LL.Node("A");
+        list.insert(node);
+        const node1 = new LL.Node("B");
+        list.insert(node1);
+        const node2 = new LL.Node("C");
+        list.insert(node2);
+        list.print();
+        list.reverse();
+        list.print();
+        expect(list.insert).toBeDefined();
+    })
+
+    it("testing linked list reverse print",()=>{
+         console.log("testing linked list reverse print");
+        const list = new LL.LinkedList();
+        const node = new LL.Node("A");
+        list.insert(node);
+        const node1 = new LL.Node("B");
+        list.insert(node1);
+        const node2 = new LL.Node("C");
+        list.insert(node2);
+        list.print();
+        list.reversePrint();
+        expect(list.insert).toBeDefined();
+    })
+
+    it("testing linked list reverse recursive",()=>{
+         console.log("testing linked list reverse recursive");
+        const list = new LL.LinkedList();
+        const node = new LL.Node("A");
+        list.insert(node);
+        const node1 = new LL.Node("B");
+        list.insert(node1);
+        const node2 = new LL.Node("C");
+        list.insert(node2);
+        list.print();
+        list.reverseRec();
+        list.print();
+        expect(list.insert).toBeDefined();
     })
 })
